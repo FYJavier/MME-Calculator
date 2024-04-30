28e0bc4c217914dbe96e0128922b46c1d1d50818
FYJavier <109763015+FYJavier@users.noreply.github.com>
2024-04-30 10:40:15 -0500

    first commit

---
+++ project.js
@@ -0,0 +1,4 @@
+let numOne = prompt("Enter the first number: ")
+let numTwo = prompt("Enter the second number:")
+let operation = parseInt(prompt("Enter the operation:"))
+document.write(`${numOne} ${operation} ${numTwo}` + "=" + `${numOne operation numTwo}`)
