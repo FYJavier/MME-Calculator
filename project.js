609fd1bebac27e4182fcef6462386706f91c9ad9
FYJavier <109763015+FYJavier@users.noreply.github.com>
2024-05-02 03:42:11 +0000

    Tried to fix updating website problem & added some logic code for calculations.

--- project.js
+++ project.js
@@ -1,5 +1,10 @@
 const numOne = document.getElementById("numOne");
 const numOneValue = numOne.value
 const numTwo = document.getElementById("numTwo");
+const numTwoValue = numTwo.value
 const operator = document.getElementById("operator");
-console.log(numOne)
+const operatorValue = operator.value
+
+if (operatorValue === "+") {
+    document.write(numOneValue + operator + numTwoValue + "=")
+}
