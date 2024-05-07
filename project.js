309d64462994ee3e5ce525908f62b45a94d5099c
FYJavier <109763015+FYJavier@users.noreply.github.com>
2024-05-07 00:42:13 +0000

    Added a calculate button, cleaned up function calculation() code, and changed heading on website.

--- project.js
+++ project.js
@@ -1,40 +1,32 @@
 const numOne = document.getElementById("numOne");
-let numOneValue = numOne.value
 const numTwo = document.getElementById("numTwo");
-const numTwoValue = numTwo.value
 const operator = document.getElementById("operator");
-const operatorValue = operator.value
+const answer = document.createElement('p')
+const body = document.querySelector('body')
+const button = document.querySelector('button')
 
 
 function calculation() {
     if (operator.value === "+") {
-        const answer = document.createElement('p')
-        const body = document.querySelector('body')
         body.append(answer);
         const sumResponse = parseInt(numOne.value) + parseInt(numTwo.value)
         return answer.innerText = numOne.value + "+" + numTwo.value + "=" + sumResponse;
     } else if (operator.value === "-"){
-        const answer = document.createElement('p')
-        const body = document.querySelector('body')
         body.append(answer);
         const sumResponse = parseInt(numOne.value) - parseInt(numTwo.value)
         return answer.innerText = numOne.value + "-" + numTwo.value + "=" + sumResponse;
     } else if (operator.value === "*") {
-        const answer = document.createElement('p')
-        const body = document.querySelector('body')
         body.append(answer);
         const sumResponse = parseInt(numOne.value) * parseInt(numTwo.value)
         return answer.innerText = numOne.value + "*" + numTwo.value + "=" + sumResponse;
     } else if (operator.value === "/") {
-        const answer = document.createElement('p')
-        const body = document.querySelector('body')
         body.append(answer);
         const sumResponse = parseInt(numOne.value) / parseInt(numTwo.value)
         return answer.innerText = numOne.value + "/" + numTwo.value + "=" + sumResponse;
     }
 }
 
-operator.addEventListener("click", e => {
+button.addEventListener("click", e => {
     calculation();
   });
 
