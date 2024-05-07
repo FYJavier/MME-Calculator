4048e35c3310f4eb7116cf148758ae8ca8664948
FYJavier <109763015+FYJavier@users.noreply.github.com>
2024-05-07 00:55:46 +0000

    Added comments

--- project.js
+++ project.js
@@ -1,3 +1,4 @@
+// Grabs elements from HTML and assigns them to variables
 const numOne = document.getElementById("numOne");
 const numTwo = document.getElementById("numTwo");
 const operator = document.getElementById("operator");
@@ -5,7 +6,7 @@ const answer = document.createElement('p')
 const body = document.querySelector('body')
 const button = document.querySelector('button')
 
-
+// Calculation occurs based on whether it's addition, subtraction, multiplication, etc.
 function calculation() {
     if (operator.value === "+") {
         body.append(answer);
@@ -26,6 +27,7 @@ function calculation() {
     }
 }
 
+// Displays answer when button is clicked
 button.addEventListener("click", e => {
     calculation();
   });
